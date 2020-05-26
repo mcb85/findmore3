1aeb96c80fa665f539c9a3e63ca5e93cb617dec4
Mariana <mariana.diaz@live.com>
2020-05-26 18:28:03 -0400

    remove build from gitignore

---
+++ client/build/precache-manifest.7d58df5f30edc7cb58d8646ad0facc7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a2bfca2636ca57a424edde5cb6b20e",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "de976aec0423f72712a7",
+    "url": "http://localhost:3000/static/css/main.487bafd8.chunk.css"
+  },
+  {
+    "revision": "d3e419fe916377100085",
+    "url": "http://localhost:3000/static/js/2.d9b936c8.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "http://localhost:3000/static/js/2.d9b936c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de976aec0423f72712a7",
+    "url": "http://localhost:3000/static/js/main.5fd94d83.chunk.js"
+  },
+  {
+    "revision": "6298fb31de7118fcdd22",
+    "url": "http://localhost:3000/static/js/runtime-main.737c55c8.js"
+  }
+]);
